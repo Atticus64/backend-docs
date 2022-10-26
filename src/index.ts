3d2f33f7c12d5de306c80b37071c10ea16bd7a0b
Jona <jonathanelian64@gmail.com>
2022-10-26 15:50:36 -0500

    add eslint configuration

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ const app = Express();
 const port = process.env.PORT ?? 8000;
 
 interface ApiObject {
-	ok: boolean,
-	msg: string
+  ok: boolean,
+  msg: string
 }
 
 app.get('/', (_req, res) => {
@@ -18,18 +18,22 @@ app.get('/', (_req, res) => {
 
 app.get('/api', (_req, res) => {
 
-	const apiStatus: ApiObject = {
-		ok: true,
-		msg: 'Todo esta bien, no se ha caido el backend',
-	}
+  const apiStatus: ApiObject = {
+    ok: true,
+    msg: 'Todo esta bien, no se ha caido el backend'
+  }
+
+  if ( apiStatus.ok === true ){
+    console.log()
+  }
 
   res.status(200).json({
-		...apiStatus
+    ...apiStatus
   });
 });
 
 app.get('*', (_req, res) => {
-	res.status(404).send('Not found')
+  res.status(404).send('Not found')
 })
 
 
