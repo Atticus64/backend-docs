26c296d4184b3e0520fa7ee19ac2efd9ca2b914f
CAMILOITT <camilocamilo5@outlook.com>
2022-10-26 22:18:26 -0500

    delete nodemond, add ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ const app = Express();
 const port = process.env.PORT ?? 8000;
 
 interface ApiObject {
-  ok: boolean,
-  msg: string
+  ok: boolean;
+  msg: string;
 }
 
 app.get('/', (_req, res) => {
@@ -17,25 +17,23 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/api', (_req, res) => {
-
   const apiStatus: ApiObject = {
     ok: true,
-    msg: 'Todo esta bien, no se ha caido el backend'
-  }
+    msg: 'Todo esta bien, no se ha caído el backend',
+  };
 
-  if ( apiStatus.ok === true ){
-    console.log()
+  if (apiStatus.ok === true) {
+    console.log();
   }
 
   res.status(200).json({
-    ...apiStatus
+    ...apiStatus,
   });
 });
 
-app.get('*', (_req, res) => {
-  res.status(404).send('Not found')
-})
-
+app.get('*', (req, res) => {
+  res.status(404).send('Not found');
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: running in http://localhost:${port}`);
