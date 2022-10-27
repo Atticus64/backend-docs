0a74debe53e56f56def00a4ed41c94ca8505c37a
CAMILOITT <camilocamilo5@outlook.com>
2022-10-26 22:51:50 -0500

    add data type

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,14 +4,14 @@ import { Sequelize } from 'sequelize';
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME || 'docs',
+  process.env.DB_NAME || 'test',
   process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD,
+  process.env.DB_PASSWORD || 'test-passwd',
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
-    port: process.env.DB_PORT || 5432,
+    port: Number(process.env.DB_PORT) || Number(5432),
     define: {
       timestamps: false,
     },
